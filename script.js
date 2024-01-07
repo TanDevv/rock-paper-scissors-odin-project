7c6c18b3b8d55ccd78655ab2f5cbf93b6c73f6b2
Tandevv <TandevUK@gmail.com>
2024-01-07 16:16:24 +0200

    add prompt, round tracking and score-keeping

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/* Scoreboard */
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
@@ -11,25 +15,50 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toUpperCase() === computerSelection) {
-    return `It's a tie! ${playerSelection.toUpperCase()} and ${computerSelection} are friends! Try again!`;
-  }
-  if (
-    (playerSelection.toUpperCase() === "ROCK" &&
-      computerSelection === "SCISSORS") ||
-    (playerSelection.toUpperCase() === "SCISSORS" &&
-      computerSelection === "PAPER") ||
-    (playerSelection.toUpperCase() == "PAPER" && computerSelection === "ROCK")
+  if (playerSelection === computerSelection) {
+    return `Tie! ${playerSelection} and ${computerSelection} are friends so they do nothing. Try again!`;
+  } else if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
+    (playerSelection == "PAPER" && computerSelection === "ROCK")
   ) {
-    return `You win! ${playerSelection.toUpperCase()} beats ${computerSelection}! Good job!`;
-  }
-  if (
-    (computerSelection === "ROCK" &&
-      playerSelection.toUpperCase() === "SCISSORS") ||
-    (computerSelection === "SCISSORS" &&
-      playerSelection.toUpperCase() === "PAPER") ||
-    (computerSelection == "PAPER" && playerSelection.toUpperCase() === "ROCK")
+    playerScore++;
+    return `You win! Your ${playerSelection} beats their ${computerSelection}!`;
+  } else if (
+    (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+    (computerSelection === "SCISSORS" && playerSelection === "PAPER") ||
+    (computerSelection == "PAPER" && playerSelection === "ROCK")
   ) {
-    return `You lose! ${computerSelection} beats ${playerSelection.toUpperCase()}! Unlucky!`;
+    computerScore++;
+    return `You lose! Thier ${computerSelection} beats your ${playerSelection}!`;
+  } else {
+    i--;
+    return "Please enter a valid input! (ROCK, PAPER or SCISSORS!)";
   }
 }
+
+function game() {
+  for (i = 0; i < 5; i++) {
+    playRound();
+    const playerSelection = prompt(
+      "Let's play a game! Choose between Rock, Paper or Scissors:"
+    ).toUpperCase();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("-------------------------------");
+  }
+  if (playerScore > computerScore) {
+    alert(
+      `You win! You won ${playerScore} rounds to their ${computerScore} rounds. The computer says good game and thinks you did well! Refresh via F5 to try again.`
+    );
+  } else if (computerScore > playerScore) {
+    alert(
+      `Aw shucks, you lose! The computer won ${computerScore} rounds to your ${playerScore} rounds. The computer says good game and hopes you enjoyed your game together! Refresh via F5 to try again.`
+    );
+  } else {
+    alert(
+      `It's a tie! You both end the game with ${playerScore} rounds won together. The computer says good game and enjoyed your time together!`
+    );
+  }
+}
+game();
